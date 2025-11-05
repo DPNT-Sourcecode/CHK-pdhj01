15a08a857659ef85e09db64209ab85609bc84a8c
sbx_user1051 <sbx_user1051@169.254.16.79>
2025-11-05 07:48:26 +0000

    Wed Nov 05 07:48:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,24 +4,25 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus: str) -> int:
-        #  - param[0] = a string containing the SKUs of all the products in the basket
-        # SKUs
         items = 0
-        if skus:
-            t = [
-                ('A', 50, '3A for 130'),
-                ('B', 30, '2B for 45'),
-                ('C', 20, None),
-                ('D', 15, None)
-                ]
+        if isinstance(skus, str):
+            for sku in skus:
+                print(sku)  
+        #     t = [
+        #         ('A', 50, '3A for 130'),
+        #         ('B', 30, '2B for 45'),
+        #         ('C', 20, None),
+        #         ('D', 15, None)
+        #         ]
             
-            #  - @return = an integer representing the total checkout value of the items
-            return sum(items)
+        #     #  - @return = an integer representing the total checkout value of the items
+        #     return sum(items)
         else:
             raise ValueError('Wrong input datatype.')
 
 
 
 c = CheckoutSolution()
-c.checkout()
+skus = 'AB'
+c.checkout(skus)
 
