4ca6785ef8861084b461ccee0e3aaa565c04b6c8
sbx_user1051 <sbx_user1051@169.254.16.79>
2025-11-05 07:45:26 +0000

    Wed Nov 05 07:45:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,27 @@
+import pandas as pd
 
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus: str) -> int:
-        t = [
-            ('A', 50, )
+        #  - param[0] = a string containing the SKUs of all the products in the basket
+        # SKUs
+        items = 0
+        if skus:
+            t = [
+                ('A', 50, '3A for 130'),
+                ('B', 30, '2B for 45'),
+                ('C', 20, None),
+                ('D', 15, None)
+                ]
+            
+            #  - @return = an integer representing the total checkout value of the items
+            return sum(items)
+        else:
+            raise ValueError('Wrong input datatype.')
 
-        # raise NotImplementedError()
+
+
+c = CheckoutSolution()
+c.checkout()
 
