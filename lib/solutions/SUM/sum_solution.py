4fd5c5580c8b53963ea04391fd51d2f26ea214dd
sbx_user1051 <sbx_user1051@169.254.16.79>
2025-11-05 07:32:23 +0000

    Wed Nov 05 07:32:23 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,7 +2,7 @@
 class SumSolution:
     
     def compute(self, x, y):
-        if x or y and 0 < x < 100 and 0 < y < 100:
+        if x or y and 0 <= x <= 100 and 0 <= y <= 100:
             return x + y
         else:
             raise ValueError(f'Invalid inputs, try again. X: {x}, Y: {y}')
