3caeccda4c6d3246c9045e8d723acfee3c3fa7b9
sbx_user1051 <sbx_user1051@169.254.16.79>
2025-11-05 07:37:00 +0000

    Wed Nov 05 07:37:00 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,6 +4,7 @@ class HelloSolution:
     # friend_name = unicode string
     def hello(self, friend_name):
         if isinstance(friend_name, str):
-            return f'Hello {friend_name}'
+            # return f'Hello {friend_name}'
+            return "Hello, World!"
         else:
             raise TypeError('Input is not a string')
