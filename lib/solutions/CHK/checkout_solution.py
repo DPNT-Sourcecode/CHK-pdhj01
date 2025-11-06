2c700fff7af06af4cfa4d687c121c10784f4bf20
sbx_user1051 <sbx_user1051@169.254.97.89>
2025-11-06 07:48:17 +0000

    Thu Nov 06 07:48:17 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,23 +18,33 @@ class CheckoutSolution:
         else:
             for sku, sku_freq in Counter(skus).items():
                 sku_freq = int(sku_freq)
+                print('sku_freq', sku_freq)
                 if inventory[sku]["promo"] is None:
                     total += inventory[sku]["price"]
                     
                 else:
+                    # Get regular price and promotion from inventory lookup
                     regular_price = inventory[sku]["price"]
                     promo = inventory[sku]["promo"]
-                    promo_qty_sku, _, promo_price = tuple(promo.split(' '))
-                    promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
-                    promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
-                    print('| promo_qty', promo_qty, '| promo_price', promo_price, '| promo_sku', promo_sku)
 
-                    if sku_freq >= promo_qty:
-                        multiplier, remainder = divmod(sku_freq, promo_qty)
+                    # Split promotion into three parts
+                    promo_qty_sku, _, promo_price = tuple(promo.split(' '))
+
+                    # Extract promo quantity and sku
+                    min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
+                    promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
+
+                    # print('| min_promo_qty', min_promo_qty, '| promo_price', promo_price, '| promo_sku', promo_sku, '| promo_sku', promo_sku)
+                    print('sku_freq', sku_freq, 'min_promo_qty', min_promo_qty)
+                    # If minimum promotion quantity exceeded, apply discount
+                    if sku_freq >= min_promo_qty:
+                        print('promo condition met')
+                        multiplier, remainder = divmod(sku_freq, min_promo_qty)
                         total = (multiplier * promo_price) + (remainder * regular_price)
 
-
+                print('total', total)
             return total
+
 
 
 
