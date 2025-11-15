1dd9845fa8f8f8d24a4605ae4d4945bd51602d97
sbx_user1051 <sbx_user1051@169.254.32.135>
2025-11-15 10:22:10 +0000

    Sat Nov 15 10:22:10 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -31,6 +31,7 @@ class CheckoutSolution:
             promo_list = []
             promo = inventory[sku]["promo"]
             discount_type = 'no_discount'
+            most_favorable = None
             if promo:
                 promos = [i.strip() for i in promo.split(',')]
                 promo_list.extend(promos)
@@ -53,8 +54,12 @@ class CheckoutSolution:
                         promo_price = None
                         discount_type = 'get_free'
 
+
                     min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
                     promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
+
+                    if most_favorable > min_promo_qty:
+                        most_favorable = min_promo_qty
 
                     promo_list[i] = {
                             'min_promo_qty':min_promo_qty,
@@ -73,6 +78,7 @@ class CheckoutSolution:
             
             # Mark discount type at SKU Level
             inventory[sku]["discount_type"] = discount_type
+            inventory[sku]["most_favorable"] = most_favorable
 
         # Use a counter to apply discounts
         total_cost = 0
@@ -93,11 +99,13 @@ class CheckoutSolution:
 
             # If bulk discount
             elif discount_type == 'bulk':
-                
+                most_favorable = 0
                 print(' bulk')
                 for promo in promos:
                     print('promo: ', promo)
-                    if promo["min_promo_qty"]
+                    # if promo["min_promo_qty"] => most_favorable:
+                    #     promo["min_promo_qty"]
+            
                     # discounts, remainder = divmod(skus[sku], promo["min_promo_qty"])
 
             # If buy X get Y free
@@ -111,6 +119,7 @@ class CheckoutSolution:
 
         print('total_cost: ', total_cost)
         return total_cost
+
 
 
 
