4b363592c18c03f4c99052dd5475dfa291710b09
sbx_user1051 <sbx_user1051@169.254.32.135>
2025-11-15 08:37:10 +0000

    Sat Nov 15 08:37:10 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,82 +32,82 @@ class CheckoutSolution:
 
         
 
-        pprint(inventory)
 
-            # else:
-            #     if promos:
-            #         for promo in promos:
-            #             # Split promotion into three parts
-            #             promo_qty_sku, _, promo_price = tuple(promo.split(' '))
+            else:
+                if promos:
+                    for promo in promos:
+                        # Split promotion into three parts
+                        promo_qty_sku, _, promo_price = tuple(promo.split(' '))
 
-            #             # Extract promo quantity and sku
-            #             min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
-            #             promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
+                        # Extract promo quantity and sku
+                        min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
+                        promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
 
-            #             inventory[sku]['min_promo_qty'] = min_promo_qty
-            #             inventory[sku]['promo_sku'] = promo_sku
-            #             inventory[sku]['promo_price'] = promo_price
+                        inventory[sku]['min_promo_qty'] = min_promo_qty
+                        inventory[sku]['promo_sku'] = promo_sku
+                        inventory[sku]['promo_price'] = promo_price
 
-            #             promo_list.append({
-            #                     'min_promo_qty':min_promo_qty,
-            #                     'promo_sku':promo_sku,
-            #                     'promo_price':promo_price
-            #                 })  
-            #         inventory[sku]['promo'] = promo_list
-            #     else:
-            #         promo_qty_sku, _, promo_price = tuple(promo.split(' '))
+                        promo_list.append({
+                                'min_promo_qty':min_promo_qty,
+                                'promo_sku':promo_sku,
+                                'promo_price':promo_price
+                            })  
+                    inventory[sku]['promo'] = promo_list
+                else:
+                    promo_qty_sku, _, promo_price = tuple(promo.split(' '))
 
-            #         # Extract promo quantity and sku
-            #         min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
-            #         promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
+                    # Extract promo quantity and sku
+                    min_promo_qty = int([i for i in promo_qty_sku if i.isnumeric()][0])
+                    promo_sku = [i for i in promo_qty_sku if i.isalpha()][0]
 
-            #         inventory[sku]['min_promo_qty'] = min_promo_qty
-            #         inventory[sku]['promo_sku'] = promo_sku
-            #         inventory[sku]['promo_price'] = promo_price
+                    inventory[sku]['min_promo_qty'] = min_promo_qty
+                    inventory[sku]['promo_sku'] = promo_sku
+                    inventory[sku]['promo_price'] = promo_price
 
-            #         promo_list.append({
-            #                 'min_promo_qty':min_promo_qty,
-            #                 'promo_sku':promo_sku,
-            #                 'promo_price':promo_price
-            #             })  
-            #         inventory[sku]['promo'] = promo_list
+                    promo_list.append({
+                            'min_promo_qty':min_promo_qty,
+                            'promo_sku':promo_sku,
+                            'promo_price':promo_price
+                        })  
+                    inventory[sku]['promo'] = promo_list
 
 
-        # # Handle non-alpha values and illegal characters
-        # if skus == '':
-        #     return 0
-        # elif not isinstance(skus, str) or skus.upper() != skus or skus == '' or not skus.isalpha():
-        #     return -1
+        # Handle non-alpha values and illegal characters
+        if skus == '':
+            return 0
+        elif not isinstance(skus, str) or skus.upper() != skus or skus == '' or not skus.isalpha():
+            return -1
         
-        # else:
-        #     # Build a counter with all the SKUs bought and their quantities
-        #     c = Counter(skus)
+        else:
+            # Build a counter with all the SKUs bought and their quantities
+            c = Counter(skus)
 
-        #     # Iterate through one item and its quantity at a time
-        #     for sku, sku_freq in c.items():
-        #         sku_freq = int(sku_freq)
+            # Iterate through one item and its quantity at a time
+            for sku, sku_freq in c.items():
+                sku_freq = int(sku_freq)
 
-        #         # Begin with non-promos
-        #         if inventory[sku]["promo"] is None:
-        #             total_no_promo += inventory[sku]["regular_price"] * sku_freq
+                # Begin with non-promos
+                if inventory[sku]["promo"] is None:
+                    total_no_promo += inventory[sku]["regular_price"] * sku_freq
                     
-        #         else:
-        #             # If minimum promotion quantity NOT exceeded
-        #             min_promo_qty = inventory[sku]['min_promo_qty']
-        #             regular_price = inventory[sku]["regular_price"]
-        #             promo_price = inventory[sku]["promo_price"]
+                else:
+                    # If minimum promotion quantity NOT exceeded
+                    min_promo_qty = inventory[sku]['min_promo_qty']
+                    regular_price = inventory[sku]["regular_price"]
+                    promo_price = inventory[sku]["promo_price"]
 
 
-        #             if sku_freq < min_promo_qty:
-        #                 total_promo += regular_price * sku_freq
+                    if sku_freq < min_promo_qty:
+                        total_promo += regular_price * sku_freq
 
-        #             # If minimum promotion quantity exceeded, apply discount
-        #             else:
-        #                 multiplier, remainder = divmod(sku_freq, min_promo_qty)
-        #                 total_promo += (int(multiplier) * int(promo_price)) + (int(remainder) * int(regular_price))
+                    # If minimum promotion quantity exceeded, apply discount
+                    else:
+                        multiplier, remainder = divmod(sku_freq, min_promo_qty)
+                        total_promo += (int(multiplier) * int(promo_price)) + (int(remainder) * int(regular_price))
             
-        #     print('input:', input, 'total_no_promo', total_no_promo, 'total_promo', total_promo)
+            print('input:', input, 'total_no_promo', total_no_promo, 'total_promo', total_promo)
 
-        #     return total_no_promo + total_promo
+            return total_no_promo + total_promo
+
 
 
